4a4aa0b23f4e08812374f186431b284b2a9c4e6f
NurseyitTursunkulov <nurseyit.tursunkukulov@gmail.com>
2020-06-18 15:26:09 +0600

    detail fragment implemented

--- app/src/main/java/com/example/makerstestapp/MainActivity.kt
+++ app/src/main/java/com/example/makerstestapp/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+//        setSupportActionBar(toolbar)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/com/example/makerstestapp/factDetails/DetailFragment.kt
+++ app/src/main/java/com/example/makerstestapp/factDetails/DetailFragment.kt
@@ -1,13 +1,14 @@
 package com.example.makerstestapp.factDetails
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import com.example.makerstestapp.R
+import com.bumptech.glide.Glide
 import com.example.makerstestapp.FactsViewModel
+import com.example.makerstestapp.R
 import kotlinx.android.synthetic.main.fragment_details.*
 import org.koin.android.viewmodel.ext.android.sharedViewModel
 
@@ -18,13 +19,20 @@ class DetailFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_details, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         factsViewModel.openDetailsEvent.observe(viewLifecycleOwner, Observer {
-            details_textview.text = it.peekContent().desc
+            val item = it.peekContent()
+            Glide.with(this).load(item.img).centerCrop().into( imageView2);
+            item_name.text = item.name
+            company_text_view.text = item.company
+            category_text_view.text = item.category
+            price_tex_view.text = item.price.toString() + "coм"
+            desc_tv.text  = item.desc
         })
     }
 }
